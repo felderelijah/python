9238c7fcb71446d2d37e27ef0a17a109dd93ff0a
felderelijah <112753774+felderelijah@users.noreply.github.com>
2024-11-30 17:47:36 -0500

    Add files via upload

--- List-Tuples-Plots/lotteryPicks.py
+++ List-Tuples-Plots/lotteryPicks.py
@@ -4,9 +4,8 @@ def lotterynumbers(): # Generate a random 6-digit lottery number
   picks = [] #empty list
   for numbers in range(6): # seven digits will be posted
     picks.append(random.randint(0, 9))
-    #picks += str(random.randint(0, 9))
   for cycle in picks:
-    print(cycle, end="")
+    print(str(cycle), end="")
   return cycle # return values 
 
 print('\tLottery Pick Numbers')
